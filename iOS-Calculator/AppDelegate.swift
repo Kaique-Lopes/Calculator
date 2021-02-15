1f7175cb2c774d262791159ca262a193c7f9b807
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2021-02-15 15:57:35 -0300

    first commit

---
+++ iOS-Calculator/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  iOS-Calculator
+//
+//  Created by Kaique Lopes on 15/02/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
