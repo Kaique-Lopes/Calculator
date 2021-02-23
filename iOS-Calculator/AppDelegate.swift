d6484c3cb03148fc1838dadfb9660078c2a4d0a0
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2021-02-23 20:57:41 -0300

    Create ViewControllers;

--- iOS-Calculator/AppDelegate.swift
+++ iOS-Calculator/AppDelegate.swift
@@ -12,7 +12,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        setupView()
         return true
+    }
+        // MARK: - Private methods
+    private func setupView() {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let vc = HomeViewController()
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
     }
 }
 
---
+++ iOS-Calculator/Controller/Home/HomeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  HomeViewController.swift
+//  iOS-Calculator
+//
+//  Created by Kaique Lopes on 23/02/21.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+        // MARK: - Initialization
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
