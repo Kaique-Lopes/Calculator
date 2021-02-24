b60bb3d27469267d37887c937b71bbefb69f9eea
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2021-02-24 12:02:53 -0300

    Add Outlets

--- iOS-Calculator/Controller/Home/HomeViewController.swift
+++ iOS-Calculator/Controller/Home/HomeViewController.swift
@@ -8,6 +8,34 @@
 import UIKit
 
 final class HomeViewController: UIViewController {
+        // MARK: - Outlets
+    
+    // Result
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    // Numbers
+    @IBOutlet weak var number0: UIButton!
+    @IBOutlet weak var number1: UIButton!
+    @IBOutlet weak var number2: UIButton!
+    @IBOutlet weak var number3: UIButton!
+    @IBOutlet weak var number4: UIButton!
+    @IBOutlet weak var number5: UIButton!
+    @IBOutlet weak var number6: UIButton!
+    @IBOutlet weak var number7: UIButton!
+    @IBOutlet weak var number8: UIButton!
+    @IBOutlet weak var number9: UIButton!
+    @IBOutlet weak var numberDecimal: UIButton!
+    
+    // Operators
+    @IBOutlet weak var operatorAC: UIButton!
+    @IBOutlet weak var operatorPlus: UIButton!
+    @IBOutlet weak var operatorPercent: UIButton!
+    @IBOutlet weak var operatorResult: UIButton!
+    @IBOutlet weak var operatorAddition: UIButton!
+    @IBOutlet weak var operatorSubstraction: UIButton!
+    @IBOutlet weak var operatorMultiplication: UIButton!
+    @IBOutlet weak var operatorDivision: UIButton!
+    
         // MARK: - Initialization
     init() {
         super.init(nibName: nil, bundle: nil)
